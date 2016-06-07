528acd4934a457a10405833804723c39c5b12fcc
Rick Daniel <rick@araishikeiwai.com>
2016-06-07 19:51:27 +0100

    searchkicked

--- app/models/section.rb
+++ app/models/section.rb
@@ -1,5 +1,17 @@
 class Section < ActiveRecord::Base
+  searchkick callbacks: :async
 
   belongs_to :document
   belongs_to :language
+
+  def search_data
+    {
+      content: content,
+      section_name: section_name,
+      country: document.country,
+      year: document.year,
+      cycle: document.cycle,
+      language: language.name
+    }
+  end
 end

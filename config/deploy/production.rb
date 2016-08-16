343001be5c98731796eb44128b2461554ece7dbc
Rick Daniel <rick@araishikeiwai.com>
2016-08-16 20:26:31 +0100

    how come

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -1,5 +1,5 @@
-role :app, %w{araishikeiwai@46.101.91.181}
-role :web, %w{araishikeiwai@46.101.91.181}
-role :db, %w{araishikeiwai@46.101.91.181}
+role :app, %w{seeker@46.101.91.181}
+role :web, %w{seeker@46.101.91.181}
+role :db, %w{seeker@46.101.91.181}
 
 server '46.101.91.181', user: 'seeker', roles: %w{web}

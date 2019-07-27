b82aa11560558de22a69d2505475f7a3d57592e0
Rosina Paige <rosina@rosinapaige.com>
2019-07-27 00:12:54 -0500

    changed the way the strengths work to fix the beas matching strongly to 'be a' issue strong - now uses .include? medium - the same weak - added word start option also added a logger
    langauges like Manx Gaelic still have inconsistancy on wether they are classed as 'strong' or 'medium'

--- app/jobs/language_parser_job.rb
+++ app/jobs/language_parser_job.rb
@@ -11,71 +11,94 @@ class LanguageParserJob
     #
     document = Document.find(id)
 
+    parse_log = Logger.new('log/parse_log.txt')
+    parse_log.debug "Log file created"
+
+
+    # languages_test_1 = ['Albanian', 'Beás','Crimean Tatar', 'Cypriot Maronite Arabic','Istro-Romanian','Kven/Finnish','Meänkieli','Scottish-Gaelic','Yiddish']
+    # languages_test_2 = ['Beás','Cornish','Irish','Manx Gaelic','Scots','Scottish-Gaelic','Ulster Scots','Welsh','Inari Sami','North Sami','South Sami','Tatar']
+    # languages_test_3 = ['Beás','Manx Gaelic']
+    # languages = []
+    #     languages_test_1.each do |language_string|
+    #       language = Language.find_by_name(language_string)
+    #       languages << language
+    #     end
+    # beas is matching on 'be a' and 'be'
+
     languages = Language.all
+
+    # strip existing language associations
+    document.sections.each do |section|
+      section.languages.clear
+    end
+
 
     strengths = [3,2,1]
 
     # this will do each part
-    # but the search will work on the entire section - split into parts
-    # so if its anywhere in the section it will turn up for all section parts
-    # and because length > 0 it'll work (probably)
-    # HOWEVER
-    # associating it with only one of the sections wont work
-    # but itll turn up for every part because of the above so itll all work out
     document.sections.each do |section|
-      logger.info section.section_number.to_s
+      parse_log.info section.section_number.to_s
       languages.each do |language|
-      logger.info '  ' + language.name.to_s
+        parse_log.info '  ' + language.name.to_s
         strengths.each do |strength|
-        logger.info '    strength' + strength.to_s
+          parse_log.info '    strength' + strength.to_s
+
           if strength == 1
             search_results = strong_search(document, language, section)
-            logger.info '      length' + search_results.length.to_s
+            parse_log.info '      length' + search_results.length.to_s
+            # if the language is in the section part
             if search_results.length > 0
-
+              # parse_log.info search_results.to_s
               if section.languages.include? language
                 relation = section.language_sections.where('language_id =' + language.id.to_s).first
                 relation.strength = 1
                 relation.save
+                parse_log.info '      post save relation.strength' + relation.strength.to_s
+                parse_log.info '      include'
               else
                 section.languages << language
                 relation = section.language_sections.where('language_id =' + language.id.to_s).first
                 relation.strength = 1
                 relation.save
+                parse_log.info '      not include'
               end
             end
 
           elsif strength == 2
             search_results = medium_search(document, language, section)
-            logger.info '      length' + search_results.length.to_s
+            parse_log.info '      length' + search_results.length.to_s
             if search_results.length > 0
 
               if section.languages.include? language
                 relation = section.language_sections.where('language_id =' + language.id.to_s).first
                 relation.strength = 2
                 relation.save
+                parse_log.info '      include'
               else
                 section.languages << language
                 relation = section.language_sections.where('language_id =' + language.id.to_s).first
                 relation.strength = 2
                 relation.save
+                parse_log.info '      not include'
               end
             end
 
           elsif strength == 3
             search_results = weak_search(document, language, section)
-            logger.info '      length' + search_results.length.to_s
+            parse_log.info '      length' + search_results.length.to_s
             if search_results.length > 0
 
               if section.languages.include? language
                   relation = section.language_sections.where('language_id =' + language.id.to_s).first
                   relation.strength = 3
                   relation.save
+                  parse_log.info '      include'
               else
                   section.languages << language
                   relation = section.language_sections.where('language_id =' + language.id.to_s).first
                   relation.strength = 3
                   relation.save
+                  parse_log.info '      not include'
                end
             end
 
@@ -89,7 +112,7 @@ class LanguageParserJob
     end
 
     Section.reindex
-    logger.info 'parsing finished'
+    parse_log.info 'parsing finished'
     document.finish_parsing!
   rescue StandardError => e
     logger.info 'Failure'
@@ -99,28 +122,18 @@ class LanguageParserJob
   end
 end
 
-
+# detect single exact words - 1
 def strong_search(document, language, section)
-  Section.search(body: {
-      "query":{
-          "bool":{
-              "must":{
-                  "dis_max":{
-                      "queries":[
-                          {"match":{"content.analyzed":{"query":language.name,"boost":10,"operator":"and","analyzer":"searchkick_search"}}}
-                      ]
-                  }
-              },
-              "filter":[
-                  {"term":{"url":document.url.to_s}},
-                  {"term":{"section_number":section.section_number}}
-              ]
-          }
-      },
-      "size":1000,
-      "from":0}).with_details
-end
 
+  if section.content.include? language.name
+    return [true]
+  else
+    return []
+  end
+  # beas as be a is a strong search match for 'be a' for some god forsaken reason
+  end
+
+# detect multiple exact words - 2
 def medium_search(document, language, section)
   Section.search(body: {
       "query":{
@@ -143,6 +156,7 @@ def medium_search(document, language, section)
       "from":0}).with_details
 end
 
+# detect multiple inexact words and word starts - 3
 def weak_search(document, language, section)
   Section.search(body: {
       "query":{
@@ -153,7 +167,8 @@ def weak_search(document, language, section)
                           {"match":{"content.analyzed":{"query":language.name,"boost":10,"operator":"and","analyzer":"searchkick_search"}}},
                           {"match":{"content.analyzed":{"query":language.name,"boost":10,"operator":"and","analyzer":"searchkick_search2"}}},
                           {"match":{"content.analyzed":{"query":language.name,"boost":1,"operator":"and","analyzer":"searchkick_search","fuzziness":1,"prefix_length":0,"max_expansions":3,"fuzzy_transpositions":true}}},
-                          {"match":{"content.analyzed":{"query":language.name,"boost":1,"operator":"and","analyzer":"searchkick_search2","fuzziness":1,"prefix_length":0,"max_expansions":3,"fuzzy_transpositions":true}}}
+                          {"match":{"content.analyzed":{"query":language.name,"boost":1,"operator":"and","analyzer":"searchkick_search2","fuzziness":1,"prefix_length":0,"max_expansions":3,"fuzzy_transpositions":true}}},
+                          {"match":{"content.word_start":{"query":language.name,"boost":10,"operator":"and","analyzer":"searchkick_word_search"}}}
                       ]
                   }
               },
